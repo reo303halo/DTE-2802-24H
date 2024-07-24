7c3c38b9f1ce2e388d4af9f8ee9f5d4730431d79
Roy Espen Olsen <roy-eo@hotmail.com>
2024-07-24 13:47:52 +0200

    sumdigits

---
+++ SumDigits/SumDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SumDigits;
+
+internal abstract class Program
+{
+    private static int SumDigits(int n)
+    {
+        if (n == 0)
+            return n;
+        
+        return n % 10 + SumDigits(n / 10);
+    }
+
+    private static void Main()
+    {
+        Console.Write("Enter an integer: ");
+        int number = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+        Console.WriteLine($"The sum of {number} is: {SumDigits(number)}");
+    }
+}
