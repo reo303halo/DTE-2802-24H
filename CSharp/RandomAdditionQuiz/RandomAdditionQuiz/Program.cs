168bffb4df4155ef1efbb4b5a54cd506364e1777
Roy Espen Olsen <roy-eo@hotmail.com>
2024-08-23 14:03:15 +0200

    Lecture 1 examples

---
+++ CSharp/RandomAdditionQuiz/RandomAdditionQuiz/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random random = new Random();
+string? continueLoop = "y";
+
+while (continueLoop == "y")
+{
+    int number1 = random.Next(0, 11);
+    int number2 = random.Next(0, 51);
+    
+    Console.Write($"What is {number1} + {number2}? ");
+    int userAnswer = int.Parse(Console.ReadLine());
+
+    Console.WriteLine(userAnswer == number1 + number2 ? "Correct!" : "Wrong answer!");
+    
+    Console.Write("Press Y to start again, or any other button to Quit: ");
+    continueLoop = Console.ReadLine()?.ToLower();
+}
+
+
+
+
+
---
+++ CSharp/UsernameGenerator/UsernameGenerator/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string? firstname = "";
+while (string.IsNullOrEmpty(firstname))
+{
+    Console.WriteLine("Please enter firstname: ");
+    firstname = Console.ReadLine();
+}
+
+string? lastname = "";
+while (string.IsNullOrEmpty(lastname))
+{
+    Console.WriteLine("Please enter lastname: ");
+    lastname = Console.ReadLine();
+}
+
+Random random = new Random();
+string randomDigits = random.Next(0, 1000).ToString("D3");
+
+string username = $"{firstname[0]}{lastname[..2]}{randomDigits}".ToLower();
+Console.WriteLine($"Your username is {username}");
