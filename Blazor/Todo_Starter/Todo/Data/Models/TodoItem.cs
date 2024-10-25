9abe94463dc6cef6a25bbfda2f65fe8cd48009ba
Roy Espen Olsen <roy-eo@hotmail.com>
2024-10-25 12:47:31 +0200

    Todo starter without API

---
+++ Blazor/Todo_Starter/Todo/Data/Models/TodoItem.cs
@@ -0,0 +1,8 @@
+namespace Todo.Data.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = "Make a todo list.";
+    public bool IsDone { get; set; } = false;
+}
